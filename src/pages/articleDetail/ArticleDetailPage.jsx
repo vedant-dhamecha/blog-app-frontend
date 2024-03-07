0250d35f66b1a3471f9dd83aa4a481cd66a423f8
vedant_dhamecha <vedantdhamecha9525@gmail.com>
2024-03-07 23:58:56 +0530

    creating loading & error state

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { generateHTML } from "@tiptap/html";
 import Bold from "@tiptap/extension-bold";
 import Document from "@tiptap/extension-document";
@@ -16,6 +16,8 @@ import { images, stables } from "../../constants";
 import SuggestedPosts from "./container/SuggestedPosts";
 import { useQuery } from "@tanstack/react-query";
 import { getSinglePost } from "../../services/index/posts";
+import ArticleDetailSkeleton from "./components/ArticleDetailSkeleton";
+import ErrorMessage from "../../components/ErrorMessage";
 
 const postsData = [
   {
@@ -55,88 +57,103 @@ const tagsData = [
 ];
 
 const ArticleDetailPage = () => {
-  const navigate = useNavigate();
   const { slug } = useParams();
   const [breadCrumbsData, setbreadCrumbsData] = useState([]);
   const [body, setBody] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
-  const { data } = useQuery({
+  const { data, isLoading: queryIsLoading, isError } = useQuery({
     queryFn: () => getSinglePost({ slug }),
-    queryKey: ['blog'],
+    queryKey: ["blog"],
   });
 
   useEffect(() => {
-    if (!slug || !data) {
-      navigate('/');
-    } else {
-      setbreadCrumbsData([
-        { name: "Home", link: "/" },
-        { name: "Blog", link: "/blog" },
-        { name: "Article title", link: `/blog/${data.slug}` },
-      ]);
+    const fetchData = async () => {
+      try {
+        if (!slug || !data) {
+          setLoading(true);
+          // Perform any additional loading state logic if needed
+        } else {
+          setbreadCrumbsData([
+            { name: "Home", link: "/" },
+            { name: "Blog", link: "/blog" },
+            { name: "Article title", link: `/blog/${data.slug}` },
+          ]);
 
-      if (data && data.body) {
-        setBody(
-          parse(
-            generateHTML(data?.body, [Bold, Italic, Text, Paragraph, Document])
-          )
-        );
+          if (data && data.body) {
+            setBody(
+              parse(
+                generateHTML(data?.body, [Bold, Italic, Text, Paragraph, Document])
+              )
+            );
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false); // Set loading to false regardless of success or failure
       }
-    }
-  }, [data, slug, navigate]);
+    };
+
+    fetchData();
+  }, [data, slug]);
 
   return (
     <MainLayout>
-      <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
-        <article className="flex-1">
-          <BreadCrumbs data={breadCrumbsData} />
-          <img
-            className="rounded-xl w-full"
-            src={
-              data?.photo
-                ? stables.UPLOAD_FOLDER_BASE_URL + data?.photo
-                : images.samplePostImage
-            }
-            alt={data?.title}
-          />
-          <div className="mt-4 flex gap-2">
-            {data?.categories.map((category) => (
-              <Link
-                to={`/blog?category=${category.name}`}
-                className="text-primary text-sm font-roboto inline-block md:text-base"
-              >
-                {category.name}
-              </Link>
-            ))}
-          </div>
-          <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
-            {data?.title}
-          </h1>
-          <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
-          <CommentsContainer className="mt-10" logginedUserId="a" />
-        </article>
-        <div>
-          <SuggestedPosts
-            header="Latest Article"
-            posts={postsData}
-            tags={tagsData}
-            className="mt-8 lg:mt-0 lg:max-w-xs"
-          />
-          <div className="mt-7">
-            <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
-              Share on:
-            </h2>
-            <SocialShareButtons
-              url={encodeURI(
-                "https://moonfo.com/post/client-side-and-server-side-explanation"
-              )}
-              title={encodeURIComponent(
-                "Client-side and Server-side explanation"
-              )}
+      {queryIsLoading ? (
+        <ArticleDetailSkeleton />
+      ) : isError ? (
+        <ErrorMessage message="Couldn't fetch the post detail" />
+      ) : (
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          <article className="flex-1">
+            <BreadCrumbs data={breadCrumbsData} />
+            <img
+              className="rounded-xl w-full"
+              src={
+                data?.photo
+                  ? stables.UPLOAD_FOLDER_BASE_URL + data?.photo
+                  : images.samplePostImage
+              }
+              alt={data?.title}
             />
+            <div className="mt-4 flex gap-2">
+              {data?.categories.map((category) => (
+                <Link
+                  to={`/blog?category=${category.name}`}
+                  className="text-primary text-sm font-roboto inline-block md:text-base"
+                >
+                  {category.name}
+                </Link>
+              ))}
+            </div>
+            <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
+              {data?.title}
+            </h1>
+            <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
+            <CommentsContainer className="mt-10" logginedUserId="a" />
+          </article>
+          <div>
+            <SuggestedPosts
+              header="Latest Article"
+              posts={postsData}
+              tags={tagsData}
+              className="mt-8 lg:mt-0 lg:max-w-xs"
+            />
+            <div className="mt-7">
+              <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
+                Share on:
+              </h2>
+              <SocialShareButtons
+                url={encodeURI("https://portfolio-2023-silk-rho.vercel.app/")}
+                title={encodeURIComponent(
+                  "Client-side and Server-side explanation"
+                )}
+              />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </MainLayout>
   );
 };
